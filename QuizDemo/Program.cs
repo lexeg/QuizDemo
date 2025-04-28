7e99f1606317a429b8a86df87ad704f515ea03de
apetrenko <apetrenko@gseis.ru>
2025-04-28 20:55:50 +0500

    Поменял прослушиваемый порт

--- QuizDemo/Program.cs
+++ QuizDemo/Program.cs
@@ -10,12 +10,14 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+}*/
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 
-app.Run();
+app.Run("http://localhost:8080");
